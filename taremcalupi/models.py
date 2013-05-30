25f82b0707588aa3e8053611881f6441e9438043
Julian Vargas <julianvargasalvarez@gmail.com>
2013-05-30 10:01:34 -0500

    adds skeletons for models

--- taremcalupi/models.py
+++ taremcalupi/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.db.models import Q
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes import generic
 
-# Create your models here.
+class Address(models.Model):
+    limit = Q(model = 'User') | Q(model = 'AffiliateUser')
+    addressable_type = models.ForeignKey(ContentType, limit_choices_to=limit)
+    addressable_id = models.PositiveIntegerField()
+    addressable_object = generic.GenericForeignKey('addressable_type', 'addressable_id')
+
+class CreditCard(models.Model):
+    pass
+
+class UserManager(models.Manager):
+    pass
+
+class User(models.Model):
+    pass
+
+class AffiliateUser(models.Model):
+    pass
+
+class AdminUser(models.Model):
+    pass
+
+class Store(models.Model):
+    pass
+
+class StoreLocation(models.Model):
+    pass
