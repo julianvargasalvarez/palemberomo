cbb6208d0230712f94a01c4ff518142ecc360829
Julian Vargas <julianvargasalvarez@gmail.com>
2013-05-31 08:41:06 -0500

    adiciona mas frontend

--- taremcalupi/static/script.js
+++ taremcalupi/static/script.js
@@ -1,10 +1,5 @@
 $(function(){
-
   $("a").on("click", function(){
     return confirm("are you sure you want to remove this?");
   });
-
-  $(".pejemaneje").css({'width': '400px', 'height': '400px'}).load('http://www.eltiempo.com/', function(){
-        alert("listo");
-      });  
 });
--- taremcalupi/views.py
+++ taremcalupi/views.py
@@ -1,13 +1,20 @@
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.list import ListView 
 from django.core.urlresolvers import reverse_lazy
+from django.http import HttpResponse
 
 from models import User
 from forms import UserForm
 
+import json
+
 class UserList(ListView):
     model = User
     template_name = 'user_list.html'
+
+    #def render_to_response(self, context, **response_kwargs):
+    #    response_kwargs['content_type'] = 'application/json'
+    #    return HttpResponse(json.dumps({'saludo': 'hola mundo'}))
 
 class UserCreate(CreateView):
     model = User
